b91404e5d58ee03c255fbc7b2f042c2948d650b1
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-03-25 15:20:09 +0530

    Info Page Updated

--- app/info/page.jsx
+++ app/info/page.jsx
@@ -1,11 +1,21 @@
 export const metadata = {
-  title: 'Info | Agyapal Singh',
+  title: "Info | Agyapal Singh",
 };
 
 const Info = () => {
   return (
-    <div>Info page</div>
-  )
-}
+    <section className="ag-info-container">
+      <div className="ag-info info-1">1</div>
+
+      <div className="ag-info info-2">2</div>
+
+      <div className="ag-info info-3">3</div>
+
+      <div className="ag-info info-4">4</div>
+
+      <div className="ag-info info-5">5</div>
+    </section>
+  );
+};
 
 export default Info;
