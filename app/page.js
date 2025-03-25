c2ff158986a5f9ff161e0028abb2d4add5f4ce51
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-03-25 13:07:48 +0530

    Home page and CSS Updated

--- app/page.js
+++ app/page.js
@@ -1,10 +1,7 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
     <div className="ag-home-container">
-      Develop Agyapal Singh
+      <h1>Developer Agyapal Singh</h1>
     </div>
   );
 }
