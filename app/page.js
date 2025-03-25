99dd15152edc67b59f8c5af8452b5e4a70468d6c
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-03-25 13:19:58 +0530

    HomePAge Updated

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
     <div className="ag-home-container">
-      <h1>Developer Agyapal Singh</h1>
+      <h1>Agyapal Singh</h1>
+      <p>
+        • Experienced web developer proficient in managing live <span> Shopify and
+        WordPress websites </span>, ensuring seamless performance and user experience.
+        <br></br>• Skilled in troubleshooting, optimizing, and implementing
+        custom features to enhance site functionality. <br></br>• Building a 
+        <span> headless Shopify solution with React.js and GraphQL APIs</span>, optimizing
+        performance and delivering a seamless user experience.
+      </p>
     </div>
   );
 }
