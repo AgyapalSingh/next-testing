9cffecc4a1641d7e121e46b3f2a4a481755e08cd
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-03-25 15:22:35 +0530

    Text of  Pages Updated

--- app/info/page.jsx
+++ app/info/page.jsx
@@ -5,15 +5,15 @@ export const metadata = {
 const Info = () => {
   return (
     <section className="ag-info-container">
-      <div className="ag-info info-1">1</div>
+      <div className="ag-info info-1">Info 1</div>
 
-      <div className="ag-info info-2">2</div>
+      <div className="ag-info info-2">Info 2</div>
 
-      <div className="ag-info info-3">3</div>
+      <div className="ag-info info-3">Info 3</div>
 
-      <div className="ag-info info-4">4</div>
+      <div className="ag-info info-4">Info 4</div>
 
-      <div className="ag-info info-5">5</div>
+      <div className="ag-info info-5">Info 5</div>
     </section>
   );
 };
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -5,15 +5,15 @@ export const metadata = {
 const Projects = () => {
   return (
     <section className="ag-projects-container">
-      <div className="ag-projects project-1">1</div>
+      <div className="ag-projects project-1">Projects 1</div>
 
-      <div className="ag-projects project-2">2</div>
+      <div className="ag-projects project-2">Projects 2</div>
 
-      <div className="ag-projects project-3">3</div>
+      <div className="ag-projects project-3">Projects 3</div>
 
-      <div className="ag-projects project-4">4</div>
+      <div className="ag-projects project-4">Projects 4</div>
 
-      <div className="ag-projects project-5">5</div>
+      <div className="ag-projects project-5">Projects 5</div>
     </section>
   );
 };
