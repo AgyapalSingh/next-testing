739cb1ea6c67daf304dbc23e2529c9d9f96093c6
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-03-25 14:58:37 +0530

    Smooth Scroll Updated

--- app/page.js
+++ app/page.js
@@ -1,15 +1,8 @@
 export default function Home() {
   return (
     <div className="ag-home-container">
-      <h1>Agyapal Singh</h1>
-      <p>
-        • Experienced web developer proficient in managing live <span> Shopify and
-        WordPress websites </span>, ensuring seamless performance and user experience.
-        <br></br>• Skilled in troubleshooting, optimizing, and implementing
-        custom features to enhance site functionality. <br></br>• Building a 
-        <span> headless Shopify solution with React.js and GraphQL APIs</span>, optimizing
-        performance and delivering a seamless user experience.
-      </p>
+      <h1>Testing Lenis</h1>
+      <p>For Smooth Scroll</p>
     </div>
   );
 }
