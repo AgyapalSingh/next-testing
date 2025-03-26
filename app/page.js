52ff066f56cde5b58cf4ee694dbc84c01ca8ce66
Agyapal Singh <agyapalsinghaujla@gmail.com>
2025-03-26 14:45:44 +0530

    Image Masking Added in H1

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div className="ag-home-container">
-      <h1>Testing Lenis</h1>
+      <div className="ag-home-title">
+        <h1>Testing Lenis</h1>
+      </div>
       <p>For Smooth Scroll</p>
     </div>
   );
